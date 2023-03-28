1b622a608dfeccdac1ae2ace44d74fd8238a6f6b
Davi <davimarque2000@gmail.com>
2023-03-28 10:51:53 -0300

    init expo project

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>fIND WEATHER!</Text>
+      <Text>FIND WEATHER!</Text>
       <StatusBar style="auto" />
     </View>
   );
